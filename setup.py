ce251bd545572d9b6c2fc268dc6dc4b5d3683794
R-Koubou <dev.r.koubou@gmail.com>
2017-02-23 23:32:42 +0900

    Add setup.py for build executable binary

---
+++ setup.py
@@ -0,0 +1,11 @@
+# encoding: utf-8
+
+import sys
+from cx_Freeze import setup, Executable
+
+exe = Executable (script = 'XLS2ExpressionMap.py', base = None)
+
+setup(  name = 'XLS2ExpressionMap',
+        version = '0.0.2',
+        description = 'Excel file(*.xlsx) to Cubase Expression Map file converter',
+        executables = [exe] )

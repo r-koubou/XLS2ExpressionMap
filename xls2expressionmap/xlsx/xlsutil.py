ced01fba80e5826c7aaebd31fc01d833c9b35678
R-Koubou <dev.r.koubou@gmail.com>
2017-08-16 05:25:43 +0900

    Organized data

--- xls2expressionmap/xlsx/xlsutil.py
+++ xls2expressionmap/xlsx/xlsutil.py
@@ -1,29 +1,9 @@
 # encoding: utf-8
 
-# MIT License
-#
-# Copyright (c) 2017 R-Koubou
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
 # http://pypi.python.org/pypi/xlrd
 import xlrd
+
+from ..util import util
 
 """
 Get a Cell array from given sheet instanse and cell string value of Row 1.
@@ -49,3 +29,18 @@ def getCellFromColmnName( sheet, rowIndex, colmnName ):
             return sheet.cell( rowIndex, c )
 
     return ""
+
+def getGroupValue( sheet, row ):
+    group = getCellFromColmnName( sheet, row, "Group" )
+
+    # Since v0.0.5: Colmn "Group" check (+backward compatibility)
+    if( group != None and hasattr( group, "value" ) ):
+        # float to int saferty
+        group = util.float2int( group.value ) - 1
+
+        if( group < 0 ):
+            group = 0
+    else:
+        group = 0
+
+    return group

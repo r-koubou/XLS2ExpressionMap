d923ee710f7f6c5e5088d89fd66a774cf299b520
R-koubou <dev.r.koubou@gmail.com>
2019-01-23 04:09:45 +0900

    Added De-Convert shell script

---
+++ ExpressionMap2Tsv.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#----------------------------------------------------------------------------
+#
+# Launch for CLI with python runtime
+#
+#----------------------------------------------------------------------------
+
+THISDIR=`dirname "${0}"`
+expr "${0}" : "/.*" > /dev/null || THISDIR=`(cd "${THISDIR}" && pwd)`
+
+XLSX=$(cd $(dirname $1 ) && pwd)/$(basename ${1})
+
+pushd $THISDIR
+    python deconvert_main.py ${XLSX}
+popd
